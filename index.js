2be9f76ad25052ced420121aaf794ef1e1d19add
KadoBloG <65702927+KadoProG@users.noreply.github.com>
2024-06-06 08:12:47 +0900

    Update index.js
    Co-authored-by: s.kadoguchi <153707392+howcollect-kadoguchi@users.noreply.github.com>

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
   const h1Element = document.createElement("h1");
   h1Element.innerText = "Hello World!";
 
+  // １秒後に要素を挿入する処理
   setTimeout(() => {
     mainElement[0].appendChild(h1Element);
   }, 1000);
